9c12c3e9126e12e7f5f908e0e8794a7b1bc6d789
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-10-25 11:17:17 -0400

    finished array basics page. Created a _partial file for the mobile table of contents.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def array_basics
-    
+    @page_title = 'Array Basics'
   end
 
   def array_methods
-
+    @page_title = 'How to Use Array Methods in Ruby'
   end
 end
